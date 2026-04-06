c4aeeb2f8da1a067725283be60d804ad3b0f2fd4
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-06 13:19:06 +0900

    FEAT: 컴포넌트 제작 - 목록 관리 페이지 헤더 - 상세 모달 취소, 수정 버튼 - 목록 테이블 컴포넌트 - main.js에 btn이 없을 경우 추가 - 어드민 공통 색상 추가

--- src/main.js
+++ src/main.js
@@ -18,12 +18,16 @@ const btn = document.getElementById("menu-btn");
 const line1 = document.getElementById("line1");
 const line2 = document.getElementById("line2");
 
-btn.addEventListener("click", () => {
-  line1.classList.toggle("translate-y-[2.5px]");
-  line1.classList.toggle("rotate-45");
+if (btn) {
+  btn.addEventListener("click", () => {
+    line1.classList.toggle("translate-y-[2.5px]");
+    line1.classList.toggle("rotate-45");
 
-  line2.classList.toggle("-translate-y-[2.5px]");
-  line2.classList.toggle("-rotate-45");
+    line2.classList.toggle("-translate-y-[2.5px]");
+    line2.classList.toggle("-rotate-45");
 
-  header.classList.toggle("is-active");
-});
+    header.classList.toggle("is-active");
+  });
+}
+
+
