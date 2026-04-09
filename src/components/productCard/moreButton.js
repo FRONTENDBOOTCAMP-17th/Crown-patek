26692f1be8a9819995af2137ee24711a214771be
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-09 14:48:20 +0900

    feat: 공통 컴포넌트 작업 - 각 페이지별로 가져오는 데이터를 카테고리 별로 분류

--- src/components/productCard/moreButton.js
+++ src/components/productCard/moreButton.js
@@ -1,10 +1,16 @@
-export async function moreButton(container) {
+export async function moreButton(container, products) {
     const response = await fetch('/src/components/productCard/moreButton.html');
     if (!response.ok) return;
+
+    const meta = products.meta;
 
     const html = await response.text();
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
+    const text = doc.body.firstElementChild;
+
+    text.querySelector(".current-products").textContent = products.data.length;
+    text.querySelector(".total-products").textContent = meta.totalCount
 
     container.appendChild(doc.body.firstElementChild);
 }
--- src/components/productCard/productCard.js
+++ src/components/productCard/productCard.js
@@ -1,10 +1,20 @@
-export async function productCard(container) {
+export async function productCard(container, products) {
     const response = await fetch('/src/components/productCard/productCard.html');
     if (!response.ok) return;
 
     const html = await response.text();
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
 
-    container.appendChild(doc.body.firstElementChild);
+    for (const product of products.data) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const card = doc.body.firstElementChild;
+        
+        card.querySelector(".product-name").textContent = product.name;
+        card.querySelector(".product-price").textContent = "₩" + product.price.toLocaleString();
+        if (product.inStock) {
+            card.querySelector(".in-stock").classList.add("hidden");
+        }
+
+        container.appendChild(card);
+    }
 }
