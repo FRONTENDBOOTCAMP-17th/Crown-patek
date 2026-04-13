c96a7ad0137dbea3d5326da4d54e2be7dda47994
jjong0 <ujjong39@gmail.com>
2026-04-13 17:39:12 +0900

    task: main.js 중복 코드 삭제

--- src/main.js
+++ src/main.js
@@ -1,18 +1,1 @@
 import "./style.css";
-
-//메뉴 버튼 이벤트
-const btn = document.getElementById("menu-btn");
-const line1 = document.getElementById("line1");
-const line2 = document.getElementById("line2");
-
-if (btn && line1 && line2 && header) {
-  btn.addEventListener("click", () => {
-    line1.classList.toggle("translate-y-[2.5px]");
-    line1.classList.toggle("rotate-45");
-
-    line2.classList.toggle("-translate-y-[2.5px]");
-    line2.classList.toggle("-rotate-45");
-
-    header.classList.toggle("is-active");
-  });
-}
