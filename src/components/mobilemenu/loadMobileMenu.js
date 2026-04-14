3feece4d2ba9b42dc3211b922272d45bda2b786c
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-14 11:41:22 +0900

    chore: 불필요 파일 삭제

--- src/components/mobilemenu/loadMobileMenu.js
+++
@@ -1,7 +0,0 @@
-export async function loadMobileMenu() {
-  const response = await fetch("/src/components/mobilemenu/mobileMenu.html");
-  if (!response.ok) return;
-
-  const html = await response.text();
-  document.body.insertAdjacentHTML("afterbegin", html);
-}
