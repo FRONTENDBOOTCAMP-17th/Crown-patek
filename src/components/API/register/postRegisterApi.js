bcac67c1f0828134a277a53da51976bc7ae9aee7
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-14 10:52:08 +0900

    feat: userData값 객체로 보관

--- src/components/API/register/postRegisterApi.js
+++ src/components/API/register/postRegisterApi.js
@@ -1,20 +1,4 @@
 import { post } from "../../../../shareApi/index.js";
 
-export const postRegisterApi = (
-  email,
-  password,
-  firstName,
-  lastName,
-  phone,
-  address,
-  addressDetail,
-) =>
-  post("/auth/signup", {
-    email,
-    password,
-    firstName,
-    lastName,
-    phone,
-    address,
-    addressDetail,
-  });
+export const postRegisterApi = ({ email, password, firstName, lastName, phone, address, addressDetail }) =>
+  post("/auth/signup", { email, password, firstName, lastName, phone, address, addressDetail });
